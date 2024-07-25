a2c8096f5e5d9e8e29134f9817da35316dec24c7
bridget <bridget.mburu.w@gmail.com>
2024-07-25 10:04:58 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+ const cats = ["Milo", "Otis", "Garfield"]
+ 
+
+const destructivelyAppendCat = () => {
+    cats.push ('Ralph')
+}
+
+const destructivelyPrependCat = () => {
+cats.unshift('Bob')
+    }
+const destructivelyRemoveFirstCat = () => {
+cats.shift()
+   }
+
+const destructivelyRemoveLastCat = () => {
+cats.pop()
+}
+
+const appendCat = (Broom)  => {
+    cats.unshift('Arnold')
+}
+
+const removeLastCat = () => {
+cats.pop()
+}
+cats.shift()
